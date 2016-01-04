f2d70c46d9c5995593a80aa20f0518e68f1c812e
Piotr Adamczak <piotr.adamczak@ovoenergy.com>
2016-01-04 11:53:49 +0000

    Uploader

---
+++ app.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var path = require('path');
+var multer  = require('multer');
+var api = require('./routes/api');
+var app = express();
+
+app.use(multer(
+    {
+        dest: './.tmp/',
+        inMemory: false
+    }
+));
+
+app.use(express.static(path.join(__dirname, 'public')));
+// logs every request
+app.use(function(req, res, next){
+    // output every request in the array
+    console.log({method:req.method, url: req.url, device: req.device});
+    next();
+});
+
+require('./routes/api')(app);
+
+var runningPortNumber = process.env.PORT || 8080;
+
+var server = app.listen(runningPortNumber, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log('Uploader app listening at http://%s:%s', host, port);
+
+});
---
+++ routes/api.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var dropbox = require("dropbox_rest");
+var fs = require('fs');
+var utils = require('utils');
+
+module.exports = function(app) {
+	/* POST photo */
+	app.post('/api/upload', upload_photo);
+};
+
+var upload_photo = function (req, res) {
+
+	if (typeof req.files.image == 'undefined') {
+		console.log('No image data');
+		res.json(400, {"error":"No image data"});
+		return;
+	}
+
+    console.log('Start uploading: %j', req.files.image.path);
+    var image = req.files.image.path;
+ 
+    var filename = 'meter_photo_'+utils.datetimestamp()+'-upload.jpg';
+    // utils.copyFileSync(image, __dirname + '/../uploads/'+filename);
+    
+    dropbox.uploadFileToDropbox(image,filename,function(error, stats) {
+    	if (error == null) {
+        	res.json(200, {"result":"Uploaded"});
+		} else {
+			res.json(400, {"error": error});
+		}
+        fs.unlink(image, function (err) {});
+    });
+}
